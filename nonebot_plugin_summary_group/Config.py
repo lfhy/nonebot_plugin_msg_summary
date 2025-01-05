a10e5a999e1006213d9f2a578d116feef0042b44
still <1772797842@qq.com>
2025-01-05 14:47:01 +0800

    modified:   nonebot_plugin_summary_group/Config.py 	modified:   nonebot_plugin_summary_group/__init__.py 	modified:   pyproject.toml

--- nonebot_plugin_summary_group/Config.py
+++ nonebot_plugin_summary_group/Config.py
@@ -12,14 +12,3 @@ class Config(BaseModel):
 
 
 config = get_plugin_config(Config)
-from nonebot import get_plugin_config
-from pydantic import BaseModel
-
-
-class Config(BaseModel):
-    gemini_key: str | None = None  # gemini接口密钥
-    summary_model: str = "gemini-1.5-flash"  # gemini模型名称
-    proxy: str | None = None  # 代理设置
-
-
-config = get_plugin_config(Config)
